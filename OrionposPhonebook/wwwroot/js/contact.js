89ac0e25d91716db38648092005c0b3ff7764ab8
erhancekin <ummugulsumerdim@gmail.com>
2023-12-26 12:12:05 +0300

    DataTables warning is fixed

--- OrionposPhonebook/wwwroot/js/contact.js
+++ OrionposPhonebook/wwwroot/js/contact.js
@@ -51,41 +51,33 @@ function Delete(id) {
     });
 }
 
-// AddContactForm submit event handler
-$('#addContactForm').submit(function (e) {
-    e.preventDefault();
 
-    // Form verilerini al
-    var formData = {
-        firstName: $('#firstName').val(),
-        lastName: $('#lastName').val(),
-        phoneNumber: $('#phoneNumber').val()
-    };
 
-    // AJAX ile yeni kayıt ekleme işlemini gerçekleştir
-    $.ajax({
-        type: 'POST',
-        url: '/Contact/AddContactForm',
-        data: JSON.stringify(formData),
-        contentType: 'application/json',
-        success: function (data) {
-            // Başarılı olduğunda modalı kapat ve tabloyu güncelle
-            $('#addContactModal').modal('hide');
-            dataTable.ajax.reload();  // DataTable'ı yeniden yükle
-            toastr.success('Yeni kayıt başarıyla eklendi.');
-        },
-        error: function (error) {
-            console.log('Error:', error);
+function deleteSelectedContacts(contactIds) {
+    Swal.fire({
+        title: 'Emin misiniz?',
+        text: 'Seçili kişileri silmek istediğinizden emin misiniz?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Evet, sil!',
+        cancelButtonText: 'İptal'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: `/Contact/RemoveContacts`,
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({ contactIds: contactIds }),
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                },
+                error: function (error) {
+                    console.log('Error:', error);
+                }
+            });
         }
     });
-});
-
-// Function to show the loading indicator
-function showLoadingIndicator() {
-    document.getElementById('loadingIndicator').style.display = 'block';
-}
-
-// Function to hide the loading indicator
-function hideLoadingIndicator() {
-    document.getElementById('loadingIndicator').style.display = 'none';
 }
